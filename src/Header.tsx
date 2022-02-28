f2329d09851761a3cbb517e586abe983e9472e24
assafmodi <assafbitonmodi@gmail.com>
2022-02-28 14:37:47 +0200

    add Sidebar

--- src/Header.tsx
+++ src/Header.tsx
@@ -5,23 +5,25 @@ import SidebarRight from './Sidebar';
 import { Box } from "@mui/material";
 import logo from './logo_ministry_of_defense.png'
 import UsersDto from "./Global/UsersDto";
+import { useTranslation } from "react-multi-lang";
 
 interface IProps {
     Item: UsersDto, 
   }
 
 export default function Header({Item}:IProps) {
-
+    const Translation = useTranslation();
     return (
         <Box style={{backgroundColor:"#FFFFFF"}}>
-            <SidebarRight />
+            <SidebarRight Item={Item} />
             <Box className={Styles.Header} >
                 <Box className={Styles.divHeader}>
                     <Box className={Styles.flex}>
-                        <Box className={Styles.flex}><Box></Box><Box style={{padding: '8px 0'}}>תפריט |</Box> </Box>
+                        <Box className={Styles.flex}><Box></Box><Box style={{padding: '8px 0'}}>{Translation('Tender.MENU')} |</Box> </Box>
                         <Box className={Styles.flex}><Box><img src={logo} width="39" height="32" alt="" /></Box><Box style={{padding: '8px 0'}}>משרד הבטחון מכרז דינאמי </Box></Box>
                     </Box>
                     <hr />
+                    <Box className={Styles.systemWatch}><Box className={Styles.title}>{Translation('Tender.SYSTEM_CLOCK')}</Box><Box className={Styles.clock}>15:46:34</Box></Box>
                 </Box>
                
             </Box>
--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -2,7 +2,13 @@ import { Button } from "@mui/material";
 import React from "react";
 import { slide as Menu } from 'react-burger-menu'
 import { Box } from "@mui/material";
-export default function SidebarRight() {
+import UsersDto from "./Global/UsersDto";
+import './index.css';
+interface IProps {
+  Item: UsersDto, 
+}
+
+export default function SidebarRight({Item}:IProps) {
 
     return (
         <Menu
@@ -15,10 +21,11 @@ export default function SidebarRight() {
           menuClassName={ "burgerMenu" }
 
         >
-          
-        <Box sx={{display:'inline-flex'}}>
-          <Box><Button variant="contained" style={{backgroundColor:'#00798C'}}  >שינוי סיסמא</Button></Box>
-          <Box><Button variant="outlined"   style={{borderColor:'#00798C', color:'#00798C'}}>התנתקות</Button></Box>
+         <Box> 
+           <Box className="UserFullName"> {Item.UserFullName}</Box>
+           <Box>
+            <Box className="logoffBtn"><Button variant="outlined"   style={{borderColor:'#00798C', color:'#00798C'}}>התנתקות</Button></Box>
+          </Box>
         </Box>
         </Menu>
       );
