22e4403968598413fea66213e811645b0f948db2
assafmodi <assafbitonmodi@gmail.com>
2022-04-07 15:29:09 +0300

    no message

--- src/features/Tender/Tender.tsx
+++ src/features/Tender/Tender.tsx
@@ -29,7 +29,7 @@ export default function Tender() {
 
   useEffect(() => {
     setOpen(true);
-    dispatch(fetchTenderAsync());
+    dispatch(fetchTenderAsync('msg'));
     dispatch(fetchLastPropositionsAsync());
     setInterval(() => {
       dispatch(fetchLastPropositionsAsync());
