9091df75a128adb4c726d6d97b828a6e160410e6
assafmodi <assafbitonmodi@gmail.com>
2022-05-04 10:23:14 +0300

    דף מכרז - הודעות קבועות קופצות #52

--- src/features/Tender/dialog.tsx
+++ src/features/Tender/dialog.tsx
@@ -1,29 +1,28 @@
 import React, { useEffect } from "react";
-import {Box,Avatar,Checkbox,ListItemAvatar,ListItemText,ListItem,List,Button,DialogTitle,DialogContentText,DialogContent,DialogActions,Dialog} from '@mui/material';
+import { Box, Avatar, Checkbox, ListItemAvatar, ListItemText, ListItem, List, Button, DialogTitle, DialogContentText, DialogContent, DialogActions, Dialog } from '@mui/material';
 import ImageIcon from '@mui/icons-material/Image';
 import { useTranslation } from "react-multi-lang";
 import UsersDto from "./../../Global/UsersDto";
 import { useDispatch } from "react-redux";
-import { setDisplayMessagesValue} from "./../Tenders/TendersSlice";
+import { setDisplayMessagesValue } from "./../Tenders/TendersSlice";
 import { useParams } from "react-router-dom";
 
 interface IProps {
     flag: boolean,
-    Messages:string[],
-    userDto:UsersDto,
+    Messages: string[],
+    userDto: UsersDto,
 }
 
-export default function DialogModel({ flag,Messages ,userDto}: IProps) {
+export default function DialogModel({ flag, Messages, userDto }: IProps) {
     const { id } = useParams();
     const dispatch = useDispatch();
     const Translation = useTranslation();
     const [open, setOpen] = React.useState(true);
     useEffect(() => {
+        if (flag){
+            setOpen(true);
+        }
 
-            if(flag){
-                setOpen(true);
-            }
-       
     }, [flag]);
     const handleClose = () => {
         setOpen(false);
@@ -39,26 +38,28 @@ export default function DialogModel({ flag,Messages ,userDto}: IProps) {
                 aria-describedby={Translation('Tender.ANNOUNCEMENTS_FOR_THIS_AUCTION')}
             >
                 <DialogTitle id="alert-dialog-title">
-                   {Translation('Tender.ANNOUNCEMENTS_FOR_THIS_AUCTION')}
+                    {Translation('Tender.ANNOUNCEMENTS_FOR_THIS_AUCTION')}
                 </DialogTitle>
                 <DialogContent>
                     <DialogContentText id="alert-dialog-description" component={'span'} >
                         <Box>
                             <List>
-                            {Messages.map((item,index) => (
-                            <ListItem key={index}>
-                                <ListItemAvatar>
-                                <Avatar>
-                                    <ImageIcon />
-                                </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText primary={item} sx={{'text-align':'right',color:'#4D4E55',fontWeight: '600'}} />
-                            </ListItem>
-                            ))}
+                                {Messages.map((item, index) => {
+                                    return item ?
+                                        <ListItem key={index}>
+                                            <ListItemAvatar>
+                                                <Avatar>
+                                                    <ImageIcon />
+                                                </Avatar>
+                                            </ListItemAvatar>
+                                            <ListItemText primary={item} sx={{ 'text-align': 'right', color: '#4D4E55', fontWeight: '600' }} />
+                                        </ListItem>
+                                        : ''
+                                    }
+                                )}
                             </List>
-
                         </Box>
-                        <Box><Checkbox defaultChecked={false}  onChange={(e)=>dispatch(setDisplayMessagesValue({tenderId:id?.replace(':','')!,display:e.target.value==='on'?false:true}))} /><span>{Translation('Tender.DO_NOT_SHOW_THIS_MESSAGE_AGAIN')}</span></Box>
+                        <Box><Checkbox defaultChecked={false} onChange={(e) => dispatch(setDisplayMessagesValue({ tenderId: id?.replace(':', '')!, display: e.target.value === 'on' ? false : true }))} /><span>{Translation('Tender.DO_NOT_SHOW_THIS_MESSAGE_AGAIN')}</span></Box>
                     </DialogContentText>
                 </DialogContent>
                 <DialogActions>
