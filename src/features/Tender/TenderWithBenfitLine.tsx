11e14d8d8af087903cea8b7f20d1ce8dee2aa664
assafmodi <assafbitonmodi@gmail.com>
2022-07-25 18:35:52 +0300

    מכרז עם העדפות - שדה מרכיב עבודה #77

--- src/features/Tender/TenderWithBenfitLine.tsx
+++ src/features/Tender/TenderWithBenfitLine.tsx
@@ -21,13 +21,13 @@ import { linePriceChanged } from "./TenderSlice";
 
 interface IProps {
   item: TenderDto,
-  itemx :TenderLineDto
+  itemx: TenderLineDto
 }
 
-export default function TenderWithBenfitLine({ item,itemx }: IProps) {
+export default function TenderWithBenfitLine({ item, itemx }: IProps) {
 
 
-          
+
   const dispatch = useDispatch();
   const userDto = useSelector(selectUser);
   const Translation = useTranslation();
@@ -36,54 +36,54 @@ export default function TenderWithBenfitLine({ item,itemx }: IProps) {
   const [price, setPrice] = useState<number>(0);
   const [valCahnge, setValChange] = React.useState('');
   const [snackbar, setSnackbar] = React.useState<IMessege>({ isOpen: false, messege: '' });
-  
+
   useEffect(() => {
 
     setValue('tenderSum', valStep);
     if (item != null) {
-        setValStep(itemx.Price);
-        setPrice(valStep);
-        reset(item);
+      setValStep(itemx.Price);
+      setPrice(valStep);
+      reset(item);
     }
-}, [item]);
+  }, [item]);
 
-const method = (value: any, helpers: any) => {
-  if (value !== itemx.PreviousPrice) {
+  const method = (value: any, helpers: any) => {
+    if (value !== itemx.PreviousPrice) {
       let Diff = Math.abs(itemx.PreviousPrice - value);
       let newPrice = Diff - itemx.PriceStep;
 
       if (newPrice < 0) {
-          let msg = value > itemx.PreviousPrice ? Translation('Tender.ValidationMsg.YOU_DIDNT_UP_STEP_AT_LAST') : Translation('Tender.ValidationMsg.YOU_DIDNT_DOWN_STEP_AT_LAST');
-          return helpers.message(msg + " " + itemx.PriceStep);
+        let msg = value > itemx.PreviousPrice ? Translation('Tender.ValidationMsg.YOU_DIDNT_UP_STEP_AT_LAST') : Translation('Tender.ValidationMsg.YOU_DIDNT_DOWN_STEP_AT_LAST');
+        return helpers.message(msg + " " + itemx.PriceStep);
       }
 
-      if(value>itemx.MaxPrice || value<itemx.MinPrice){
-          setValStep(itemx.PreviousPrice);
-          setValue('tenderSum', itemx.PreviousPrice, { shouldValidate: true })
-          dispatch(linePriceChanged({ TenderLineId: itemx.TenderLineId, Price: itemx.PreviousPrice }));
+      if (value > itemx.MaxPrice || value < itemx.MinPrice) {
+        setValStep(itemx.PreviousPrice);
+        setValue('tenderSum', itemx.PreviousPrice, { shouldValidate: true })
+        dispatch(linePriceChanged({ TenderLineId: itemx.TenderLineId, Price: itemx.PreviousPrice }));
       }
-  }
-  return value;
-};
+    }
+    return value;
+  };
 
-const schema = Joi.object({
-  tenderSum: Joi.
+  const schema = Joi.object({
+    tenderSum: Joi.
       number().//message('מספר').
       positive().//message('חיובי').
       precision(2).//message('2 דצימלי').
       min(Math.max(2, itemx.MinPrice)).message(Translation('Tender.ValidationMsg.MinPriceErr') + " (" + itemx
-      .MinPrice + ")").
+        .MinPrice + ")").
       max(itemx.MaxPrice).message(Translation('Tender.ValidationMsg.MaxPriceErr') + " (" + itemx.MaxPrice + ")").
       //multiple(item.PriceStep).message('min.invalid').
       custom(method, 'custom validation').
       required()//.message('דרוש');
 
-}).options({ allowUnknown: true });//instead of adding non shown fields
+  }).options({ allowUnknown: true });//instead of adding non shown fields
 
-const { register, handleSubmit, reset, setValue, trigger, formState: { errors } } = useForm({
-  resolver: joiResolver(schema),
-  mode: 'onBlur', //'onBlur' 'onChange' 'onSubmit'[*default] 'onTouched' 'all'
-});
+  const { register, handleSubmit, reset, setValue, trigger, formState: { errors } } = useForm({
+    resolver: joiResolver(schema),
+    mode: 'onBlur', //'onBlur' 'onChange' 'onSubmit'[*default] 'onTouched' 'all'
+  });
 
   return (
     <React.Fragment>
@@ -99,7 +99,7 @@ const { register, handleSubmit, reset, setValue, trigger, formState: { errors } 
                         <Grid className={Styles.Title} item >{itemx.TenderLineName}</Grid>
                         <Grid className={Styles.amount} item >
                           {!item.IsPercentageCalculation ?
-                            <label>{Translation('Tender.NUMBER_OF_UNITS')}</label>
+                            <label>{Translation('Tender.AMOUNT')}</label>
                             : <label>{Translation('Tender.WEIGHT')}</label>}
                           &nbsp;{itemx.RequiredAmount}</Grid>
                       </Grid>
@@ -111,55 +111,50 @@ const { register, handleSubmit, reset, setValue, trigger, formState: { errors } 
                         <Grid item className={Styles.unitPrice}>
                           <Grid item>{Translation('Tender.UNIT_PRICE_MUST_BE_ENTERED')}</Grid>
                           <Grid item container className={Styles.TextField}>
-                            <Grid item ><IconButton sx={{ color: "#00798C" }}  
-                                          
-                                            onClick={() => {
-                                                let val = valCahnge ? parseFloat(valCahnge) : itemx.Price;
-                                                if (val > itemx.MaxPrice) {
-                                                    setSnackbar({ isOpen: true, messege: Translation('Tender.PRICE_IS_HIGHER_THAN_THE_MAXIMUM') });
-                                                };
+                            <Grid item ><IconButton sx={{ color: "#00798C" }}
+                              onClick={() => {
+                                let val = valCahnge ? parseFloat(valCahnge) : itemx.Price;
+                                if (val > itemx.MaxPrice) {
+                                  setSnackbar({ isOpen: true, messege: Translation('Tender.PRICE_IS_HIGHER_THAN_THE_MAXIMUM') });
+                                };
 
-                                                const newVal = Number(valStep) + Number(itemx.PriceStep);
-                                                setValStep(newVal);
-                                                setValue('tenderSum', newVal, { shouldValidate: true });
+                                const newVal = Number(valStep) + Number(itemx.PriceStep);
+                                setValStep(newVal);
+                                setValue('tenderSum', newVal, { shouldValidate: true });
 
-                                                dispatch(linePriceChanged({ TenderLineId: itemx.TenderLineId, Price: newVal }));
-                                            }}><AddCircle /></IconButton></Grid>
+                                dispatch(linePriceChanged({ TenderLineId: itemx.TenderLineId, Price: newVal }));
+                              }}><AddCircle /></IconButton></Grid>
                             <Grid item className={Styles.divTextField}>
                               <TextField
                                 defaultValue={valStep ? Number(valStep).toFixed(2) : ''}
                                 {...register('tenderSum')} placeholder={itemx.CurrencyId}
-                                            //value={valStep}
+                                onChange={(e) => {
+                                  setValStep(Number(e.target.value))
+                                  dispatch(linePriceChanged({ TenderLineId: itemx.TenderLineId, Price: e.target.value }));
+                                }}
 
-                                            onChange={(e) => {
-                                                setValStep(Number(e.target.value))
-                                                dispatch(linePriceChanged({ TenderLineId: itemx.TenderLineId, Price: e.target.value }));
-                                            }}
-
-                                            onBlur={(e) => {
-
-                                                setValue('tenderSum', e.target.value, { shouldValidate: true })
-                                                dispatch(linePriceChanged({ TenderLineId: itemx.TenderLineId, Price: e.target.value }));
-                                            }}
-                                            type={'text'}
-                                            label={Translation('Tender.PRICE_PER_UNIT') + ' ' + item.CurrencyId}
+                                onBlur={(e) => {
+                                  setValue('tenderSum', e.target.value, { shouldValidate: true })
+                                  dispatch(linePriceChanged({ TenderLineId: itemx.TenderLineId, Price: e.target.value }));
+                                }}
+                                type={'text'}
                                 size="small"
                                 variant="filled"
                                 margin='normal'
                                 fullWidth
                               ></TextField></Grid>
                             <Grid item ><IconButton sx={{ color: "#00798C" }} onClick={() => {
-                                        let val = valCahnge ? parseFloat(valCahnge) : itemx.Price;
-                                        if (val < itemx.MinPrice || val < 0) {
-                                            setSnackbar({ isOpen: true, messege: Translation('Tender.PRICE_IS_LOWER_THAN_THE_MINIMUM') });
-                                        }
-                                        const newVal = Number(valStep) - Number(itemx.PriceStep);
-                                        setValStep(newVal);
-                                        setValue('tenderSum', newVal, { shouldValidate: true });
+                              let val = valCahnge ? parseFloat(valCahnge) : itemx.Price;
+                              if (val < itemx.MinPrice || val < 0) {
+                                setSnackbar({ isOpen: true, messege: Translation('Tender.PRICE_IS_LOWER_THAN_THE_MINIMUM') });
+                              }
+                              const newVal = Number(valStep) - Number(itemx.PriceStep);
+                              setValStep(newVal);
+                              setValue('tenderSum', newVal, { shouldValidate: true });
 
-                                        dispatch(linePriceChanged({ TenderLineId: itemx.TenderLineId, Price: newVal }));
-                                    }
-                                    }><RemoveCircle /></IconButton></Grid>
+                              dispatch(linePriceChanged({ TenderLineId: itemx.TenderLineId, Price: newVal }));
+                            }
+                            }><RemoveCircle /></IconButton></Grid>
                           </Grid>
                         </Grid>
                         <Grid item >
@@ -209,17 +204,17 @@ const { register, handleSubmit, reset, setValue, trigger, formState: { errors } 
                           <Grid item justifyContent="center" className={Styles.item}>
                             <Grid item justifyContent="center" className={Styles.title}>בניכוי העדפה לאומית</Grid>
                             <Grid item justifyContent="center" className={Styles.summery}><CurrencyFormat displayType={'text'} thousandSeparator={true} decimalScale={2} />
-                              {itemx.TotalWithLocationBenefit }</Grid>
+                              {itemx.TotalWithLocationBenefit}</Grid>
                           </Grid>
                           <Grid item justifyContent="center" className={Styles.item}>
                             <Grid item justifyContent="center" className={Styles.title}>בניכוי תוצרת הארץ</Grid>
                             <Grid item justifyContent="center" className={Styles.summery}><CurrencyFormat displayType={'text'} thousandSeparator={true} decimalScale={2} />
-                            {itemx.TotalForLocalWork }</Grid>
+                              {itemx.TotalForLocalWork}</Grid>
                           </Grid>
                           <Grid item justifyContent="center" className={Styles.item}>
                             <Grid item justifyContent="center" className={Styles.title}>{Translation('Tender.THE_AMOUNT_OF_YOUR_BID')}</Grid>
                             <Grid item justifyContent="center" className={Styles.summery}><CurrencyFormat displayType={'text'} thousandSeparator={true} decimalScale={2} />
-                            {itemx.TotalWithoutBenefits }</Grid>
+                              {itemx.TotalWithoutBenefits}</Grid>
                           </Grid>
                         </Grid>
                         <Grid container justifyContent="center" className={Styles.buttonDiv}>
